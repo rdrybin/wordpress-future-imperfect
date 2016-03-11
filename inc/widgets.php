6ea88ac65eb47bb807d6c92176d6652df98300b4
Topher <topher@x-team.com>
2016-03-10 21:59:20 -0500

    Begin new posts widget

--- inc/widgets.php
+++ inc/widgets.php
@@ -5,6 +5,13 @@
  * @package Future_Imperfect
  */
 
+// register future_imperfect widgets
+function register_future_imperfect_widgets() {
+    register_widget( 'Future_Imperfect_Title_Widget' );
+    register_widget( 'Future_Imperfect_Large_Post_List_Widget' );
+}
+add_action( 'widgets_init', 'register_future_imperfect_widgets' );
+
 /**
  * Adds Future_Imperfect_Title_Widget widget.
  */
@@ -74,8 +81,111 @@ class Future_Imperfect_Title_Widget extends WP_Widget {
 
 } // class Future_Imperfect_Title_Widget
 
-// register Future_Imperfect_Title_Widget widget
-function register_future_imperfect_title_widget() {
-    register_widget( 'Future_Imperfect_Title_Widget' );
-}
-add_action( 'widgets_init', 'register_future_imperfect_title_widget' );
+
+/**
+ * Adds Future_Imperfect_Large_Post_List_Widget widget.
+ */
+class Future_Imperfect_Large_Post_List_Widget extends WP_Widget {
+
+	/**
+	 * Register widget with WordPress.
+	 */
+	function __construct() {
+		parent::__construct(
+			'future_imperfect_large_post_list_widget', // Base ID
+			__( 'Future Imperfect Large Post List', 'future-imperfect' ), // Name
+			array( 'description' => __( 'Lists posts with a large icon', 'future-imperfect' ), ) // Args
+		);
+	}
+
+	/**
+	 * Go get the data
+	 *
+	 * @param array $args     Query arguments.
+	 */
+	function get_data( $args = '' ) {
+
+		$output = '';
+
+		$args = array(
+			'posts_per_page' => 5,
+		);
+
+		// The Query
+		$the_query = new WP_Query( $args );
+
+		// The Loop
+		if ( $the_query->have_posts() ) {
+			$output .= '<ul>';
+			while ( $the_query->have_posts() ) {
+				$the_query->the_post();
+				$output .= '<li>' . get_the_title() . '</li>';
+			}
+			$output .= '</ul>';
+		} else {
+			// no posts found
+		}
+		/* Restore original Post Data */
+		wp_reset_postdata();
+
+		return $output;
+
+	}
+
+	/**
+	 * Front-end display of widget.
+	 *
+	 * @see WP_Widget::widget()
+	 *
+	 * @param array $args     Widget arguments.
+	 * @param array $instance Saved values from database.
+	 */
+	public function widget( $args, $instance ) {
+		echo $args['before_widget'];
+		if ( ! empty( $instance['title'] ) ) {
+			echo $args['before_title'] . apply_filters( 'widget_title', $instance['title'] ). $args['after_title'];
+		}
+
+		// list posts
+
+		echo $this->get_data();
+
+
+		echo $args['after_widget'];
+	}
+
+	/**
+	 * Back-end widget form.
+	 *
+	 * @see WP_Widget::form()
+	 *
+	 * @param array $instance Previously saved values from database.
+	 */
+	public function form( $instance ) {
+		$title = ! empty( $instance['title'] ) ? $instance['title'] : __( 'New title', 'future-imperfect' );
+		?>
+		<p>
+		<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label> 
+		<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>">
+		</p>
+		<?php 
+	}
+
+	/**
+	 * Sanitize widget form values as they are saved.
+	 *
+	 * @see WP_Widget::update()
+	 *
+	 * @param array $new_instance Values just sent to be saved.
+	 * @param array $old_instance Previously saved values from database.
+	 *
+	 * @return array Updated safe values to be saved.
+	 */
+	public function update( $new_instance, $old_instance ) {
+		$instance = array();
+		$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
+
+		return $instance;
+	}
+
+} // class Future_Imperfect_Large_Post_List_Widget
