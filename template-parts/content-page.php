6aa4156c612d4ec58be17d6df61c2990c6f4841e
rdrybin <rdrybin@yandex.ru>
2022-10-20 13:23:15 +0500

    add tag list

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -2,24 +2,24 @@
 <article class="post">
 	<header>
 		<div class="title">
-			<h2><a href="<?php echo esc_url( get_permalink() ); ?>"><?php echo esc_html( get_the_title() ); ?></a></h2>
+			<h2><a href="<?php echo esc_url(get_permalink()); ?>"><?php echo esc_html(get_the_title()); ?></a></h2>
 
 			<?php
-				// check to see if we have subtitles
-				if ( function_exists( 'get_the_subtitle' ) ) {
-					echo '<p>' . get_the_subtitle( get_the_ID(), '', '', false ) . '</p>' . "\n";
-				}
+			// check to see if we have subtitles
+			if (function_exists('get_the_subtitle')) {
+				echo '<p>' . get_the_subtitle(get_the_ID(), '', '', false) . '</p>' . "\n";
+			}
 			?>
 
 		</div>
 	</header>
-	
+
 	<?php
-		if ( has_post_thumbnail() ) {
-			echo '<a href="' . esc_url( get_permalink() ) . '" class="image featured">';
-			the_post_thumbnail( 'future-imperfect-large' ); 
-			echo '</a>';
-		}
+	if (has_post_thumbnail()) {
+		echo '<a href="' . esc_url(get_permalink()) . '" class="image featured">';
+		the_post_thumbnail('future-imperfect-large');
+		echo '</a>';
+	}
 	?>
 
 	<?php the_content(); ?>
@@ -29,9 +29,9 @@
 			<li></li>
 		</ul>
 		<ul class="stats">
-			<li><?php echo get_the_category_list( ',' ); ?></li>
-			<?php // <li><a href="#" class="icon fa-heart">28</a></li> ?>
-			<li><a href="<?php comments_link(); ?>" class="icon fa-comment"><?php comments_number( '0', '1', '%' ); ?></a></li>
+			<li><?php echo get_the_category_list(','); ?></li>
+			<li><?php echo get_the_tag_list(','); ?></li>
+			<li><a href="<?php comments_link(); ?>" class="icon fa-comment"><?php comments_number('0', '1', '%'); ?></a></li>
 		</ul>
 	</footer>
 </article>
